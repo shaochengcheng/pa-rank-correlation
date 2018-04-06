b7283b02e22c252a297174309c0449a37cc20c2a
Chengcheng Shao <shaoc@indiana.edu>
2018-04-06 12:17:28 +0800

    ready to bg2

--- pa_test.py
+++ pa_test.py
@@ -1,14 +1,19 @@
 from rank_correlation import g_one_main
-import numpy as np
+import sys
 
 
-def main_test(N):
-    n = 2 ** N
-    for md in (1, 10):
-	for model in ('pa', 'configuration'):
-	    g_one_main(n, md, model
+def main_test(N, nrounds=1):
+    n = 2**N
+    for i in range(nrounds):
+        for md in (1, 10):
+            for model in ('pa', 'configuration'):
+                g_one_main(n, md, model)
 
 
 if __name__ == '__main__':
-    if
-    pa_mpi()
+    if len(sys.argv) == 2:
+        main_test(int(sys.argv[1]))
+    elif len(sys.argv) == 3:
+        main_test(int(sys.argv[1]), int(sys.argv[2]))
+    else:
+        print('Only one or two parameters are allowed')
